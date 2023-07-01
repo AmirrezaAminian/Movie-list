42d1c0d3f738c267a2798309f682239ca0140da9
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-07-01 15:48:31 +0330

    Fixing props

--- src/App.js
+++ src/App.js
@@ -55,18 +55,25 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main movies={movies}>
+        <ListBox movies={movies}>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
-function NavBar({movies}) {
+function NavBar({children}) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   );
 }
@@ -102,16 +109,15 @@ function NumResults({ movies }) {
   );
 }
 
-function Main({movies}) {
+function Main({children}) {
   return (
     <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
+      {children}
     </main>
   );
 }
 
-function ListBox({movies}) {
+function ListBox({children}) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -122,7 +128,7 @@ function ListBox({movies}) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
