bb9ff54166d511e25a2e798f0242b21538ce2f34
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-07-01 14:34:47 +0330

    component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,97 +50,21 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "f84fc31d";
-
 export default function App() {
-  const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [selectedId, setSelectedId] = useState(null);
-  const tempQuery = "Mad Max";
-
-  useEffect(function(){
-    ''
-  })
-  
-
-
-  useEffect(function () {
-    async function fetchMovies() {
-      try {
-        setIsLoading(true);
-        setError('')
-        const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-        );
-
-        if (!res.ok)
-          throw new Error("Something went wrong with fetching movies");
-
-          
-          const data = await res.json();
-          if(data.Response === 'False'){
-              throw new Error ('Movie not found')
-          }
-        setMovies(data.Search);
-      } catch (err) {
-        setError(err.message);
-      }finally {
-        setIsLoading(false);
-      }
-    }
-
-
-    if(!query.length < 3){
-      setMovies([]);
-      setError('')
-      return
-    }
-
-
-    fetchMovies();
-  }, [query]);
-
   return (
     <>
-      <NavBar>
-        <Search  query={query} setQuery={setQuery} />
-        <Numresults movies={movies} />
-      </NavBar>
-      <Main>
-        <Box>
-          {/* {isLoading ? <Loader /> : <MovieList movies={movies} />}</Box> */}
-          {isLoading && <Loader />}
-          {!isLoading && !error && <MovieList movies={movies} />}
-          {error && <ErrorMessage message={error} />}
-        </Box>
-        <Box>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched} />
-        </Box>
-      </Main>
+      <NavBar />
+      <Main />
     </>
   );
 }
 
-function Loader() {
-  return <p className="loader">Loading...</p>;
-}
-
-function ErrorMessage({ message }) {
-  return (
-    <p className="error">
-      <span>⛔</span> {message}
-    </p>
-  );
-}
-function NavBar({ children }) {
+function NavBar() {
   return (
     <nav className="nav-bar">
       <Logo />
-      {children}
+      <Search />
+      <NumResults />
     </nav>
   );
 }
@@ -154,7 +78,8 @@ function Logo() {
   );
 }
 
-function Search({query , setQuery}) {
+function Search() {
+  const [query, setQuery] = useState("");
 
   return (
     <input
@@ -167,57 +92,42 @@ function Search({query , setQuery}) {
   );
 }
 
-function Numresults({ movies }) {
+function NumResults() {
   return (
     <p className="num-results">
-      Found <strong>{movies.length}</strong> results
+      Found <strong>X</strong> results
     </p>
   );
 }
 
-function Main({ children }) {
-  return <main className="main">{children}</main>;
-}
-
-function Box({ children }) {
-  const [isOpen, setIsOpen] = useState(true);
-
+function Main() {
   return (
-    <div className="box">
-      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
-        {isOpen ? "–" : "+"}
-      </button>
-      {isOpen && children}
-    </div>
+    <main className="main">
+      <ListBox />
+      <WatchedBox />
+    </main>
   );
 }
 
-/*
-function WatchedBox() {
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen2, setIsOpen2] = useState(true);
+function ListBox() {
+  const [isOpen1, setIsOpen1] = useState(true);
 
   return (
     <div className="box">
       <button
         className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
+        onClick={() => setIsOpen1((open) => !open)}
       >
-        {isOpen2 ? "–" : "+"}
+        {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen2 && (
-        <>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched} />
-        </>
-      )}
+      {isOpen1 && <MovieList />}
     </div>
   );
 }
 
-*/
+function MovieList() {
+  const [movies, setMovies] = useState(tempMovieData);
 
-function MovieList({ movies }) {
   return (
     <ul className="list">
       {movies?.map((movie) => (
@@ -242,11 +152,32 @@ function Movie({ movie }) {
   );
 }
 
+function WatchedBox() {
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen2((open) => !open)}
+      >
+        {isOpen2 ? "–" : "+"}
+      </button>
+      {isOpen2 && (
+        <>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </>
+      )}
+    </div>
+  );
+}
+
 function WatchedSummary({ watched }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
-
   return (
     <div className="summary">
       <h2>Movies you watched</h2>
@@ -282,25 +213,25 @@ function WatchedMoviesList({ watched }) {
   );
 }
 
-function WatchedMovie({ movie }) {
+function WatchedMovie({movie}){
   return (
-    <li>
-      <img src={movie.Poster} alt={`${movie.Title} poster`} />
-      <h3>{movie.Title}</h3>
-      <div>
-        <p>
-          <span>⭐️</span>
-          <span>{movie.imdbRating}</span>
-        </p>
-        <p>
-          <span>🌟</span>
-          <span>{movie.userRating}</span>
-        </p>
-        <p>
-          <span>⏳</span>
-          <span>{movie.runtime} min</span>
-        </p>
-      </div>
-    </li>
-  );
+    <li >
+          <img src={movie.Poster} alt={`${movie.Title} poster`} />
+          <h3>{movie.Title}</h3>
+          <div>
+            <p>
+              <span>⭐️</span>
+              <span>{movie.imdbRating}</span>
+            </p>
+            <p>
+              <span>🌟</span>
+              <span>{movie.userRating}</span>
+            </p>
+            <p>
+              <span>⏳</span>
+              <span>{movie.runtime} min</span>
+            </p>
+          </div>
+        </li>
+  )
 }
